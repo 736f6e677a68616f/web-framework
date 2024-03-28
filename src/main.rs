e14952179a84e7617dce0cdcc13ee262f8cb1f98
songzhao <songzhao@shangyoo.com>
2024-03-28 10:12:58 +0800

    111

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,21 @@
-use std::{cell::RefCell, collections::VecDeque, rc::Rc, result};
 fn main() {
-    
 }
-fn is_valid_bst(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
-    fn helper(root: Option<Rc<RefCell<TreeNode>>>, lower: i32, upper: i32) -> bool {
-        if let Some(node) = root {
-            let node = node.borrow();
-            if node.val <= lower || node.val >= upper {
-                return false;
-            } else {
-                helper(node.left.clone(), lower, node.val) && helper(node.right.clone(), node.val, upper)
-            }
-        } else {
-            true
-        }
-    }
-    helper(root, i32::MIN - 1, i32::MAX + 1)
+fn spiral_matrix(m: i32, n: i32, head: Option<Box<ListNode>>) -> Vec<Vec<i32>> {
+
 }
 
-#[derive(Debug, PartialEq, Eq)]
-pub struct TreeNode {
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
   pub val: i32,
-  pub left: Option<Rc<RefCell<TreeNode>>>,
-  pub right: Option<Rc<RefCell<TreeNode>>>,
+  pub next: Option<Box<ListNode>>
 }
 
-impl TreeNode {
+impl ListNode {
   #[inline]
-  pub fn new(val: i32) -> Self {
-    TreeNode {
-      val,
-      left: None,
-      right: None
+  fn new(val: i32) -> Self {
+    ListNode {
+      next: None,
+      val
     }
   }
 }
