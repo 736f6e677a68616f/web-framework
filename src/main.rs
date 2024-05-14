1083c487142907bcec72bacef25e9de5d5f8a0c7
songzhao <songzhao@shangyoo.com>
2024-05-14 11:30:08 +0800

    1111

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 use anyhow::Result;
-use tokio::fs;
+use redis::AsyncCommands;
+use random;
+use random::Source;
+
 #[tokio::main]
-async fn main() -> Result<()>{
-    let f = fs::read("G:/a/src/aaa.txt").await?;
-    
-    println!("{:?}", String::from_utf8(f));
+async fn main() -> Result<()> {
+    let _ = init_rds().await?;
+    Ok(())
+}
+
+async fn init_rds() -> Result<()> {
+    let cli = redis::Client::open("redis://192.168.16.109:6379")?;
+    let mut con = cli.get_multiplexed_async_connection().await?;
+    let mut source = random::default(42);
+    let num = source.read::<f64>();
+    con.set("key", num).await?;
+    let r = con.get("key").await?;
+    dbg!(r);
     Ok(())
 }
